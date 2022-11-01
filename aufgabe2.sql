45fbba23765ff5d78b74eff050c3be1c26f54e12
danisltpi <lcdns@protonmail.com>
2022-11-01 18:44:37 +0100

    aufgabe 2 wip

--- aufgabe2.sql
+++ aufgabe2.sql
@@ -1,8 +1,58 @@
 SELECT auftrnr,
   datum
 FROM auftrag
-WHERE kundnr = (
+WHERE kundnr IN (
     SELECT nr
     FROM kunde
     WHERE name = 'Fahrrad Shop'
-  )
+  );
+SELECT auftrnr,
+  datum
+FROM auftrag
+WHERE kundnr = SOME (
+    SELECT nr
+    FROM kunde
+    WHERE name = 'Fahrrad Shop'
+  );
+SELECT auftrnr,
+  datum
+FROM auftrag
+WHERE EXISTS(
+    SELECT *
+    FROM kunde
+    WHERE auftrag.kundnr = (
+        SELECT nr
+        FROM kunde
+        WHERE name = 'Fahrrad Shop'
+      )
+  );
+SELECT kundnr,
+  COUNT(*) AS anzahl,
+  MIN(datum) AS von,
+  MAX(datum) AS bis
+FROM auftrag
+GROUP BY kundnr;
+SELECT kundnr,
+  COUNT(*) AS anzahl,
+  MIN(datum) AS von,
+  MAX(datum) AS bis
+FROM auftrag
+GROUP BY kundnr
+HAVING COUNT(*) = 1;
+SELECT kunde.nr,
+  kunde.name,
+  auftrag.auftrnr AS auftrag,
+  personal.name
+FROM auftrag
+  JOIN kunde ON auftrag.kundnr = kunde.nr
+  JOIN personal ON auftrag.persnr = personal.persnr;
+SELECT * FROM (
+
+-- kinda sus
+SELECT kunde.name,
+  COUNT(auftrag.auftrnr) as anzahl
+FROM kunde
+  JOIN auftrag ON auftrag.kundnr = kunde.nr
+  GROUP BY kunde.name
+  HAVING anzahl= MAX(anzahl)
+) 

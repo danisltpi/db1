b0d830119ffee47234fb74eb68384df9c661222c
LunaVarra <linnahe13@gmail.com>
2022-11-06 18:02:29 +0100

    Formatierung & Löschen unnötige Query Aufgabe 1

--- aufgabe1.sql
+++ aufgabe1.sql
@@ -2,17 +2,15 @@ SELECT * FROM teilestamm;
 
 SELECT * FROM teilestamm WHERE UPPER(bezeichnung) LIKE UPPER('%city%');
 
-SELECT * from auftragsposten;
-
 SELECT teilnr, gesamtpreis 
 FROM auftragsposten
 WHERE gesamtpreis = (SELECT MAX(gesamtpreis) FROM auftragsposten);
 
-SELECT COUNT(*) as kunden from kunde;
-SELECT COUNT(*) from personal;
-SELECT COUNT(*) from teilestamm;
+SELECT COUNT(*) AS kunden FROM kunde;
+SELECT COUNT(*) FROM personal;
+SELECT COUNT(*) FROM teilestamm;
 
-SELECT MIN(datum) as von, MAX(datum) as bis from auftrag
+SELECT MIN(datum) AS von, MAX(datum) AS bis FROM auftrag
 
 --- Auftrag 2 kunde: Maier Ingrid, Personal: Johannes Köster 
 SELECT * FROM auftrag WHERE kundnr = 3;
@@ -21,9 +19,8 @@ SELECT name FROM personal WHERE persnr = 1;
 
 SELECT * FROM lager WHERE bestand > 0 ORDER BY bestand;
 
--- sus
 SELECT DISTINCT teilnr FROM lieferung ORDER BY teilnr DESC;
 
 SELECT teilnr AS nummer, bezeichnung, preis AS brutto FROM teilestamm WHERE  preis > 30;
 
-SELECT einzelteilnr AS teilennummer FROM teilestruktur WHERE oberteilnr = 300001 AND anzahl > 100
+SELECT einzelteilnr AS teilennummer FROM teilestruktur WHERE oberteilnr = 300001 AND anzahl > 100;
